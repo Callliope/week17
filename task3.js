b2f5ddaf7349a95bf8299cf97d220cfd403daf5d
Maria Mihailovskaya <mihailovskayam33@gmail.com>
2022-05-31 17:26:17 +0300

    task 3 done

--- task3.js
+++ task3.js
@@ -1,15 +1,28 @@
-function formatDate(){
-    var date = new Date();
-    var now=new Date();
-    var diff =date-now;
+var date=new Date();
+function formatDate(date){ 
+    var diff =new Date()-date;
     if (diff<1000){
-        alert("прямо сейчас")
+        alert(diff+" прямо сейчас"); 
     }
     else if (diff<60000){
         alert(Math.floor(diff/1000)+ " сек. назад")
 
     }
+    else if (diff<3600000){
+        alert(Math.floor(diff/60000)+" мин.назад")
+    }
+    else{
+        alert( formatDate(new Date(new Date - 86400* 4 * 1000)) );
+    }
+
+
+//      alert( formatDate(new Date(new Date - 30 * 1000)) ); // "30 сек. назад"
+
+//     alert( formatDate(new Date(new Date - 5 * 60 * 1000)) ); // "5 мин. назад"
+
+// // вчерашняя дата вроде 31.12.2016, 20:00
+//     alert( formatDate(new Date(new Date - 86400* 4 * 1000)) );
 
 
 }
-formatDate()
+formatDate(date);
