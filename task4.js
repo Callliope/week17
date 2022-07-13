6d26cc0ce4b26078848221592f4fd68675c76092
Maria Mihailovskaya <mihailovskayam33@gmail.com>
2022-07-13 16:29:37 +0300

    task 4 add func

--- task4.js
+++ task4.js
@@ -1,9 +1,9 @@
 var numPool = [-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10 ];
 
 function randomize(arr) {
-//   for (let i = 0; i < 10; i++){
-    // a=numPool.sort(()=>Math.random()-0.5)
+  for (let i = 0; i < 10; i++){
+    a=numPool.sort(()=>Math.random()-0.5)
     return arr.map(i=>[Math.random(), i]).sort().map(i=>i[1]);
     randomize(numPool);
-//   }  
+  }  
 };
